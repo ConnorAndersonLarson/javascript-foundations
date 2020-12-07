05ae6790c5df21aa0949dae66918f6ba529bd17a
Connor Anderson Larson <Connorandersonlarson@gmail.com>
2020-12-07 17:57:01 -0600

    complete unicorn.js

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,8 +1,29 @@
+//completed
+//talked through
+//erased + completed again, 12.7
+
 class Unicorn {
   constructor(name, color) {
-    this.name = name;
-    this.color = color;
+    this.name = name
+    if (color === undefined) {
+      this.color = 'white'
+    } else {
+      this.color = color
+    }
   }
+
+  isWhite() {
+    if (this.color === 'white') {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  says(words) {
+    return `**;* ${words} *;**`
+  }
+
 }
 
 module.exports = Unicorn;
