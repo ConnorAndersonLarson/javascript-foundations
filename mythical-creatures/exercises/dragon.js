9fd3c47096d22b61a945f93aa85949eba122074f
Connor Anderson Larson <Connorandersonlarson@gmail.com>
2020-12-07 20:35:37 -0600

    complete dragon.js

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,23 @@
+class Dragon {
+  constructor(name, rider) {
+    this.name = name,
+    this.rider = rider,
+    this.hungry = true
+    this.hunger = 0
+  }
 
+  greet() {
+    return `Hi, ${this.rider}!`
+  }
+
+  eat() {
+    this.hunger += 1
+    if (this.hunger >= 3) {
+      this.hungry = false
+    }
+  }
+
+}
+
+
+module.exports = Dragon;
